431b8b56856d1425510fb43785cba609009f9cda
leesoonwoo <lswteen@gmail.com>
2023-11-06 17:20:18 +0900

    [main] modify : ELK 구조를위한 프로젝트 작업

--- domain/src/main/java/com/pick/domain/document/ProductDocument.java
+++ domain/src/main/java/com/pick/domain/document/ProductDocument.java
@@ -5,14 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Getter
-@Document(indexName = "product_index")
+@Document(indexName = "product-index")
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDocument {
     @Id
     private Long productId;
+    @Field(name = "name", type = FieldType.Text)
     private String name;
+    @Field(name = "price", type = FieldType.Double)
     private Double price;
 }
